8ec91fc95ab6f44057e7db6679158a50d787cf2e
UItimate <52617311+UItimate@users.noreply.github.com>
2022-06-21 23:38:06 -0600

    Add playRound and userPlay functions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
+let computerScore = 0;
+let playerScore = 0;
 
+playerSelection = userPlay();
+computerSelection = computerPlay();
 
 function computerPlay() {
-    const itemsArray = ["Rock", "Paper", "Scissors"];
-    let randomItem = itemsArray[Math.floor(Math.random() * itemsArray.length)];
-    console.log(randomItem)
+    itemsArray = ["rock", "paper", "scissors"];
+    computerSelection = itemsArray[Math.floor(Math.random() * itemsArray.length)];
+    console.log(computerSelection)
+    return computerSelection;
 }
 
-computerPlay()
+function playRound() {
+    if (playerSelection == computerSelection) {
+        console.log(playerSelection + " " + computerSelection + " You both tie!")
+    }
+    (computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "scissors" && playerSelection == "paper" || playerSelection == "rock" && computerSelection == "paper") {
+        console.log("You lose! Rock beats scissors " + computerScore++);
+    }
+    if (computerSelection == "scissors" && playerSelection == "rock" || computerSelection == "paper" && playerSelection == "scissors" || computerSelection == "paper" && playerSelection == "rock") {
+        console.log("You win! Scissors beats rock " + playerScore++);
+    }
+}
 
+function userPlay() {
+    playerSelection = prompt("Rock, paper, or scissors?").toLowerCase()
+    console.log(playerSelection)
+    return playerSelection;
+}
+
+playRound()
